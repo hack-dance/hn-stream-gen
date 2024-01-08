43c8d58c7ca91c105919e4b9cbb8a8051fafc815
Dimitri Kennedy <dimitrikennedy@gmail.com>
2024-01-08 04:45:59 -0500

    oops

--- src/constants/modes.ts
+++
@@ -1,9 +0,0 @@
-export const MODE = {
-  FUNCTIONS: "FUNCTIONS",
-  TOOLS: "TOOLS",
-  JSON: "JSON",
-  MD_JSON: "MD_JSON",
-  JSON_SCHEMA: "JSON_SCHEMA"
-} as const
-
-export type MODE = keyof typeof MODE
